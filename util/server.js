5ca0c9ffc9a53fe2f12c90bd1fb0bbd2943c6177
Tavius Koktavy <tavius@nyu.edu>
2024-03-25 00:59:01 -0400

    Use localhost + linting

--- util/server.js
+++ util/server.js
@@ -4,9 +4,9 @@ import * as path from 'path';
 
 let baseDirectory = '.';
 let port = 8080;
-let host = '0.0.0.0';
+let host = 'localhost';
 let lasttRequesTime = performance.now() / 1000;
-for(let i = 0; i < process.argv.length; ++i) {
+for (let i = 0; i < process.argv.length; ++i) {
   if (process.argv[i] == '-d' && i < process.argv.length - 1) {
     baseDirectory = process.argv[i + 1];
   }
@@ -15,7 +15,7 @@ for(let i = 0; i < process.argv.length; ++i) {
   }
   if (process.argv[i] == '-h' && i < process.argv.length - 1) {
     host = process.argv[i + 1];
-  } 
+  }
 }
 
 function sleep(ms) {
@@ -25,37 +25,37 @@ function sleep(ms) {
 }
 
 http
-  .createServer( async function (request, response) {
+  .createServer( async function(request, response) {
 
-    response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-    response.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    response.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    response.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
 
     let filePath = baseDirectory + request.url;
 
     const extname = path.extname(filePath);
-    let contentType = "text/html";
+    let contentType = 'text/html';
     switch (extname) {
-      case ".js":
-        contentType = "text/javascript";
+      case '.js':
+        contentType = 'text/javascript';
         break;
-      case ".css":
-        contentType = "text/css";
+      case '.css':
+        contentType = 'text/css';
         break;
-      case ".json":
-        contentType = "application/json";
+      case '.json':
+        contentType = 'application/json';
         break;
-      case ".png":
-        contentType = "image/png";
+      case '.png':
+        contentType = 'image/png';
         break;
-      case ".jpg":
-        contentType = "image/jpg";
+      case '.jpg':
+        contentType = 'image/jpg';
         break;
     }
 
     const requestTime = performance.now() / 1000;
     if (requestTime - lasttRequesTime > 1) {
-      console.log("");
-      console.log("-----------------------------------------------");
+      console.log('');
+      console.log('-----------------------------------------------');
     }
 
     let queryString;
@@ -66,20 +66,20 @@ http
     }
 
     let testDirectory = filePath;
-    if (testDirectory.endsWith("/")) {
+    if (testDirectory.endsWith('/')) {
       testDirectory = testDirectory.substring(0, testDirectory.length - 1);
     }
     try {
       if (fs.lstatSync(filePath).isDirectory()) {
         let testDirectory = filePath;
-        if (!testDirectory.endsWith("/")) testDirectory = testDirectory + "/";
-        if (fs.existsSync(testDirectory + "index.html")) {
-          filePath = testDirectory + "index.html";
-        } else if (fs.existsSync(testDirectory + "index.htm")) {
-          filePath = testDirectory + "index.htm";
+        if (!testDirectory.endsWith('/')) testDirectory = testDirectory + '/';
+        if (fs.existsSync(testDirectory + 'index.html')) {
+          filePath = testDirectory + 'index.html';
+        } else if (fs.existsSync(testDirectory + 'index.htm')) {
+          filePath = testDirectory + 'index.htm';
         }
       }
-    } catch(err) {
+    } catch (err) {
       // ignore
     }
 
@@ -87,34 +87,34 @@ http
       const stats = fs.statSync(filePath);
       if (stats && stats.size) {
         const fileSizeInBytes = stats.size;
-        response.setHeader("Content-Length", fileSizeInBytes);
+        response.setHeader('Content-Length', fileSizeInBytes);
       }
-    } catch(err) {
+    } catch (err) {
       // ignore
     }
 
-    fs.readFile(filePath, async function (error, content) {
+    fs.readFile(filePath, async function(error, content) {
       if (error) {
-        if (error.code == "ENOENT") {
-          console.log("HTTP(404) Request for " + filePath + " -> File not found.");
+        if (error.code == 'ENOENT') {
+          console.log('HTTP(404) Request for ' + filePath + ' -> File not found.');
         } else {
-          console.log("HTTP(500)) Request for " + filePath + " -> Server error.");
+          console.log('HTTP(500)) Request for ' + filePath + ' -> Server error.');
           response.writeHead(500);
           response.end(
-            "Sorry, check with the site admin for error: " +
+            'Sorry, check with the site admin for error: ' +
               error.code +
-              " ..\n"
+              ' ..\n'
           );
           response.end();
         }
       } else {
-        console.log("HTTP(200) Request for " + filePath);
-        response.writeHead(200, { "Content-Type": contentType });
-        response.end(content, "utf-8");
+        console.log('HTTP(200) Request for ' + filePath);
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
       }
     });
 
     lasttRequesTime = requestTime;
   })
   .listen(port, host);
-console.log("Server running at " + host + ':' + port);
+console.log('Server running at ' + host + ':' + port);
